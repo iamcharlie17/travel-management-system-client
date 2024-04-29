9ee90f7835991a08e5b0f07c6a89dd4600221c49
Riyad Hosen <riyadhosen@syn-172-220-155-225.res.spectrum.com>
2024-04-28 20:53:37 -0700

    added onsubmit on add spot form and collected data

--- src/Pages/AddSpots.jsx
+++ src/Pages/AddSpots.jsx
@@ -1,13 +1,49 @@
 const AddSpots = () => {
+  const handleAddSpot = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.name.value;
+    const email = form.email.value;
+    const touristSpotName = form.touristSpotName.value;
+    const countryName = form.countryName.value;
+    const location = form.location.value;
+    const shortDescription = form.shortDescription.value;
+    const averageCost = form.averageCost.value;
+    const season = form.season.value;
+    const travelTime = form.travelTime.value;
+    const visitorsPerYear = form.visitorsPerYear.value;
+    const imageUrl = form.imageUrl.value;
+
+    const touristSpotInfo = {
+      userName,
+      email,
+      touristSpotName,
+      countryName,
+      location,
+      shortDescription,
+      averageCost,
+      season,
+      travelTime,
+      visitorsPerYear,
+      imageUrl,
+    };
+    console.log(touristSpotInfo);
+
+    form.reset();
+  };
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center gap-2 pt-16">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Add Tourist Spots</h1>
       </div>
 
-      <form className="md:grid  md:grid-cols-2 gap-4 w-full md:w-2/3  p-8 shadow-xl mx-auto bg-white rounded-xl">
+      <form
+        onSubmit={handleAddSpot}
+        className="md:grid  md:grid-cols-2 gap-4 w-full md:w-2/3  p-8 shadow-xl mx-auto bg-white rounded-xl"
+      >
         <div>
-          <label htmlFor="userName">Your Name</label>
+          <label htmlFor="name">Your Name</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
@@ -18,115 +54,111 @@ const AddSpots = () => {
           />
         </div>
         <div>
-          <label htmlFor="userName">Your Email</label>
+          <label htmlFor="email">Your Email</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
-            type="text"
-            name="name"
+            type="email"
+            name="email"
             placeholder="Enter your Email"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Tourist spot name</label>
+          <label htmlFor="touristSpotName">Tourist spot name</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="touristSpotName"
             placeholder="Enter tourist spot name"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Country Name</label>
+          <label htmlFor="countryName">Country Name</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="countryName"
             placeholder="Enter country name"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Location</label>
+          <label htmlFor="location">Location</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="location"
             placeholder="Enter location"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Short description</label>
+          <label htmlFor="shortDescription">Short description</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="shortDescription"
             placeholder="Enter short description"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Average cost</label>
+          <label htmlFor="averageCost">Average cost</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="averageCost"
             placeholder="Enter average cost"
             id=""
           />
         </div>
         <div>
-          <label
-            htmlFor="userName"
-          >
-            Seasonality
-          </label>
+          <label htmlFor="season">Seasonality</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="season"
             placeholder="Enter season"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Travel time</label>
+          <label htmlFor="travelTime">Travel time</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="travelTime"
             placeholder="Enter travel time in days"
             id=""
           />
         </div>
         <div>
-          <label htmlFor="userName">Vistors per year</label>
+          <label htmlFor="visitorsPerYear">Vistors per year</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="visitorsPerYear"
             placeholder="Enter visitors per year"
             id=""
           />
         </div>
         <div className="col-span-2 w-full">
-          <label htmlFor="userName">Image URL</label>
+          <label htmlFor="imageUrl">Image URL</label>
           <br />
           <input
             className="w-full px-4 border-2 rounded-l py-1"
             type="text"
-            name="name"
+            name="imageUrl"
             placeholder="Enter spot's image url"
             id=""
           />
